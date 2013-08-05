d94e312e4deef081db40022034ffe19565efadb3
squirrelfm <squirrelfm@gmail.com>
2013-08-05 21:28:56 +0400

    Missing protocol erro

---
+++ Shared/SharedProtocol/Exceptions/ProtocolError.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using SharedProtocol.Framing;
+
+namespace SharedProtocol.Exceptions
+{
+    /// <summary>
+    /// Generic protocol error exception.
+    /// </summary>
+    public class ProtocolError: Exception
+    {
+        public ResetStatusCode Code { get; set; }
+
+        public ProtocolError(ResetStatusCode code, string message): base(message)
+        {
+            Code = code;
+        }
+    }
+}
