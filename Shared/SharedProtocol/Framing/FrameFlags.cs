2f7c76ea451ac1a52e0f0636c6ae948903c2e8c1
SquirrelFm <squirrelfm@gmail.com>
2013-08-16 13:30:15 +0400

    Fixed Issue #22. Changed pong flag to 1 from 2.

--- Shared/SharedProtocol/Framing/FrameFlags.cs
+++ Shared/SharedProtocol/Framing/FrameFlags.cs
@@ -7,7 +7,7 @@ namespace SharedProtocol.Framing
     {
         None = 0x00,
         EndStream = 0x01,
-        Pong = 0x02,
+        Pong = 0x01,
         EndHeaders = 0x04,
         Priority = 0x08,
     }
