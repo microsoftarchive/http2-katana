af04494bac82e5e135b702276fa7b5827e642b32
sgrebnov <sergei.grebnov@gmail.com>
2013-08-23 14:05:54 +0400

    Tests. adds webApi test project

---
+++ Tests/Http2.Owin.Server.WebApi.Tests/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.Owin.Hosting;
+using Owin.Test.WebApiTest;
+using SocketServer;
+
+namespace Http2.Server.Owin.WebApi.Tests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectString = "https://localhost:8443/";
+
+            var startOpt = new StartOptions(connectString)
+            {
+                ServerFactory = typeof(SocketServerFactory).ToString(),
+            };
+            
+
+            try
+            {
+                // Start OWIN host 
+                using (WebApp.Start<Startup>(startOpt))
+                {
+                    // Create HttpCient and make a request to api/values 
+                    //HttpClient client = new HttpClient();
+
+                    //var response = client.GetAsync(baseAddress + "api/values").Result;
+
+                    //Console.WriteLine(response);
+                    //Console.WriteLine(response.Content.ReadAsStringAsync().Result);
+                } 
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(String.Format("Error => {0} : {1}", 
+                    ex.Message, 
+                    (ex.InnerException != null) ? ex.InnerException.Message : String.Empty));
+           
+            }
+
+            Console.ReadLine(); 
+        } 
+    }
+}
---
+++ Tests/Http2.Owin.Server.WebApi.Tests/Startup.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Http; 
+
+
+namespace Owin.Test.WebApiTest
+{
+    public class Startup
+    {
+        // This code configures Web API. The Startup class is specified as a type
+        // parameter in the WebApp.Start method.
+        public void Configuration(IAppBuilder appBuilder)
+        {
+            // Configure Web API for self-host. 
+            HttpConfiguration config = new HttpConfiguration();
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            appBuilder.UseHttp2();
+            appBuilder.UseWebApi(config);
+        }
+    }
+}
