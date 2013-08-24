044d5d1ce09ac97de688610a89768d064f40b46e
sgrebnov <sergei.grebnov@gmail.com>
2013-08-24 17:51:03 +0400

    Server. Http11Manager clean up

--- Shared/SharedProtocol/Http11/Http11Manager.cs
+++ Shared/SharedProtocol/Http11/Http11Manager.cs
@@ -18,169 +18,8 @@ namespace SharedProtocol.Http11
     /// </summary>
     public static class Http11Manager
     {
-        /// <summary>
-        /// Download Successful event
-        /// </summary>
-        public static event EventHandler<Http11ResourceDownloadedEventArgs> OnDownloadSuccessful;
-
-        /// <summary>
-        /// Socket closed event
-        /// </summary>
-        public static event EventHandler<SocketCloseEventArgs> OnSocketClosed;
-
-
-        //Remove file:// from Assembly.GetExecutingAssembly().CodeBase
-        private static readonly string AssemblyPath =
-            Path.GetDirectoryName(Assembly.GetExecutingAssembly().CodeBase.Substring(8));
-        
-        private static string GetFileName(IEnumerable<string> headers)
-        {
-            string getRequest = String.Empty;
-
-            //Finding Get request - filename is there
-            foreach (var header in headers)
-            {
-                if (header.IndexOf("GET", StringComparison.OrdinalIgnoreCase) != -1)
-                {
-                    getRequest = header;
-                    break;
-                }
-            }
-
-            var getRequestSplitted = getRequest.Split(new [] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
-
-            foreach (var token in getRequestSplitted.Where(token => token.StartsWith("/")))
-            {
-                return token;
-            }
-
-            return String.Empty;
-        }
-
-        private static void SaveFile(string directory, string fileName, byte[] fileBytes)
-        {
-            string newfilepath;
-
-            // create local file path
-            if (!string.IsNullOrEmpty(directory))
-            {
-                if (directory[0] == '\\')
-                {
-                    directory = '.' + directory;
-                }
-
-                Directory.CreateDirectory(directory);
-                newfilepath = directory + '\\' + fileName;
-            }
-            else
-            {
-                newfilepath = fileName;
-            }
-
-            if (File.Exists(newfilepath))
-            {
-                try
-                {
-                    File.Delete(newfilepath);
-                }
-                catch (Exception)
-                {
-                    Http2Logger.LogError("Cant overwrite file: " + newfilepath);
-                }
-            }
-            using (var fs = new FileStream(newfilepath, FileMode.Create))
-            {
-                fs.Write(fileBytes, 0, fileBytes.Length);
-            }
-
-            Http2Logger.LogInfo("File saved: " + fileName);
-        }
-
-        private static string[] ReadHeaders(SecureSocket socket)
-        {
-            var lineBuffer = new byte[256];
-            string header = String.Empty;
-            int totalBytesCame = 0;
-            int bytesOfLastHeader = 0;
-            var headers = new List<string>(10);
-
-            while (true)
-            {
-                bool gotException = false;
-                var bf = new byte[1];
-                int bytesCame = socket.Receive(bf);
-                if (bytesCame == 0) break;
-
-                Buffer.BlockCopy(bf, 0, lineBuffer, totalBytesCame, bytesCame);
-                totalBytesCame += bytesCame;
-                try
-                {
-                    header = Encoding.UTF8.GetString(lineBuffer, bytesOfLastHeader, totalBytesCame - bytesOfLastHeader);
-                }
-                catch
-                {
-                    gotException = true;
-                }
-
-                if (totalBytesCame != 0 && !gotException && header[header.Length - 1] == '\n')
-                {
-                    headers.Add(header.TrimEnd('\n', '\r'));
-                    bytesOfLastHeader = totalBytesCame;
-                }
-
-                // empty header means we got \r\n\r\n which was trimmed. This means end of headers block.
-                if (headers.Count >= 2 && String.IsNullOrEmpty(headers.LastOrDefault()))
-                {
-                    break;
-                }
-            }
-
-            headers.RemoveAll(String.IsNullOrEmpty);
-
-            return headers.ToArray();
-        }
-
-        public static void Http11DownloadResource(SecureSocket socket, Uri requestUri)
-        {
-            byte[] headersBytes = Encoding.UTF8.GetBytes(ConstructHeaders(requestUri));
-            int sent = socket.Send(headersBytes);
-
-            string[] responseHeaders = ReadHeaders(socket);
-
-            var buffer = new byte[128 * 1024]; //128 kb
-            using (var stream = new MemoryStream(128 * 1024))
-            {
-                while (true)
-                {
-                    int received = socket.Receive(buffer, 0, buffer.Length, SocketFlags.None);
-                    if (received == 0)
-                        break;
-
-                    stream.Write(buffer, 0, received);
-                }
-
-                var fileBuffer = new byte[stream.Position];
-                Buffer.BlockCopy(stream.GetBuffer(), 0, fileBuffer, 0, fileBuffer.Length);
-                int fileNameIndex = requestUri.AbsolutePath.LastIndexOf("/");
-                string fileName = requestUri.AbsolutePath.Substring(fileNameIndex);
-
-                string directory = AssemblyPath;
-                SaveFile(directory, fileName, fileBuffer);
-
-                if (OnDownloadSuccessful != null)
-                {
-                    OnDownloadSuccessful(null, new Http11ResourceDownloadedEventArgs(fileBuffer.Length, fileName));
-                }
-
-                socket.Close();
-
-                if (OnSocketClosed != null)
-                {
-                    OnSocketClosed(null, new SocketCloseEventArgs());
-                }
-            }
-        }
-
+      
+   
         //public static void Http11SendResponse(SecureSocket socket)
         //{
         //    string[] headers = GetHttp11Headers(socket);
@@ -230,11 +69,13 @@ namespace SharedProtocol.Http11
         //    }
         //}
 
-        public static int SendResponse(DuplexStream socket, byte[] data, int statusCode, string contentType)
+        public static int SendResponse(DuplexStream socket, byte[] data, int statusCode, string contentType, Dictionary<string,string> headers = null)
         {
             string initialLine = "HTTP/1.1 " + statusCode + " " + StatusCode.GetReasonPhrase(statusCode) + "\r\n";
 
-            var headers = new Dictionary<string,string>();
+            if (headers == null)
+                headers = new Dictionary<string,string>();
+
             if (data.Length > 0)
             {
                 headers.Add("Content-Type", contentType);
@@ -283,7 +124,7 @@ namespace SharedProtocol.Http11
             socket.Write(headersBytes);
         }
 
-        public static string[] GetHttp11Headers(DuplexStream socket)
+        public static string[] ReadHeaders(DuplexStream socket)
         {
             var headers = new List<string>(5);
 
