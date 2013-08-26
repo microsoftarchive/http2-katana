ca0de60d6684d3e58fdd097c507d65b6e264f110
sgrebnov <sergei.grebnov@gmail.com>
2013-08-26 15:53:26 +0400

    Server. Adds Upgrade support for Http11ProtocolOwinAdapter

--- Server/SocketServer/OwinAdapter/Http11ProtocolOwinAdapter.cs
+++ Server/SocketServer/OwinAdapter/Http11ProtocolOwinAdapter.cs
@@ -12,50 +12,78 @@ using SharedProtocol.Http11;
 using SharedProtocol;
 using SharedProtocol.IO;
 using SharedProtocol.Utils;
+using SharedProtocol.Handshake;
 
 namespace SocketServer
 {
     using AppFunc = Func<IDictionary<string, object>, Task>;
     using Environment = IDictionary<string, object>;
+    using UpgradeDelegate = Action<IDictionary<string, object>, Func<IDictionary<string, object>, Task>>;
 
+    /// <summary>
+    /// Implements Http11 protocol handler.
+    /// Converts request to OWIN environment, triggers OWIN pipilene and then sends response back to the client.
+    /// </summary>
     public static class Http11ProtocolOwinAdapter
     {
+        /// <summary>
+        /// Request parameters read timeout.
+        /// </summary>
+        private const int ReadTimeout = 6000; // TODO use global constants instead
+
+        /// <summary>
+        /// Processes incoming request.
+        /// </summary>
+        /// <param name="client">The client connection.</param>
+        /// <param name="environment">The OWIN environment</param>
+        /// <param name="next">The next component in the pipeline.</param>
         public static async void ProcessRequest(DuplexStream client, Environment environment, AppFunc next)
         {
+            // args checking
+            if (client == null)
+            {
+                throw new ArgumentNullException("client");
+            }
+
             try
             {
-                if (!client.CanRead || !client.WaitForDataAvailable(500)) return;
+                // invalid connection, skip
+                if (!client.CanRead || !client.WaitForDataAvailable(ReadTimeout)) return;
 
-
-                // TODO check for upgrade and add coresponding settings
-
-                // upgrade 1.1->2.0 detection must be there
-
-                string[] rawHeaders = Http11Manager.ReadHeaders(client);
+                var rawHeaders = Http11Manager.ReadHeaders(client);
                 // TODO - review if OwinRequest constructor can do this for us.
 
                 Http2Logger.LogDebug("Http1.1 Protocol Handler. Process request " + string.Join(" ", rawHeaders));
-
+                
+                // invalid connection, skip
                 if (rawHeaders == null || rawHeaders.Length == 0) return;
 
                 // headers[0] contains METHOD, URI and VERSION like "GET /api/values/1 HTTP/1.1"
-                var headers = ParseHeaders(rawHeaders.Skip(1).ToArray());
+                var headers = Http11Manager.ParseHeaders(rawHeaders.Skip(1).ToArray());
 
+                // parse request parameters: method, path, host, etc
                 string[] splittedRequestString = rawHeaders[0].Split(' ');
                 string method = splittedRequestString[0];
                 string scheme = client.Socket.SecureProtocol == SecureProtocol.None ? "http" : "https";
                 string host = headers["Host"][0]; // client MUST include Host header due to HTTP/1.1 spec
                 if (host.IndexOf(':') == -1)
                 {
-                    host += ":80"; // use default port
+                    host += (scheme == "http" ? ":80" : ":443"); // use default port
                 }
-
                 string path = splittedRequestString[1];
 
                 // TODO get body from request
+
+                // main owin environment components
                 var env = CreateOwinEnvironment(method, scheme, host, "", path);
 
-                await next(env);
+                // we may need to populate additional fields if request supports UPGRADE
+                AddOpaqueUpgradeIfNeeded(headers, env);
+                
+                if (next != null)
+                {
+                    await next(env);
+                }
 
                 EndResponse(client, env);
             }
@@ -69,41 +97,110 @@ namespace SocketServer
             }
         }
 
-        // TODO find better way
-        private static IDictionary<string, string[]> ParseHeaders(string[] headers)
-        {
-            var dict = new Dictionary<string, string[]>();
-            foreach (var header in headers)
-            {
-                int colonIndex = header.IndexOf(':');
-                if (colonIndex == -1)
-                {
-                    dict.Add(header, new string[0]);
-                }
-                else
-                {
-                    string headerName = header.Substring(0, colonIndex);
-                    string[] values = header.Substring(colonIndex + 2).Split(','); // colon and space are skipped
-                    for (int i = 0; i < values.Length; ++i)
-                    {
-                        values[i] = values[i].Trim();
-                    }
+        #region Opague Upgrade
 
-                    dict.Add(headerName, values);
-                }
+        /// <summary>
+        /// Implements Http1 to Http2 apgrade delegate according to 'OWIN Opaque Stream Extension'.
+        /// </summary>
+        /// <param name="env">The OWIN environment.</param>
+        /// <param name="next">The callback delegate to be executed after Opague Upgarde is done.</param>
+        private static void OpaqueUpgradeDelegate(Environment env, AppFunc next)
+        {
+            try
+            {
+                // TODO UpgradeHandshaker.Handshake method must be refactored
+                UpgradeHandshaker.Handshake(env);
+            }
+            finally
+            {
+                next(env);
+            }
+        }
+
+        /// <summary>
+        /// Inspects request headers to see if supported UPGRADE method is defined; if so, specifies Opague.Upgarde delegate as per 'OWIN Opaque Stream Extension'.
+        /// </summary>
+        /// <param name="headers">Parsed request headers.</param>
+        /// <param name="env">The OWIN request paremeters to update.</param>
+        private static void AddOpaqueUpgradeIfNeeded(IDictionary<string, string[]> headers, IDictionary<string, object> env)
+        {
+            if (!headers.ContainsKey("Upgrade") || !headers.ContainsKey("HTTP2-Settings"))
+            {
+                return;
             }
 
-            return dict;
+            // make sure we are upgrading to HTTP/2.0 protocol (version may also be HTTP-draft-04/2.0 or similar)
+            if (headers["Upgrade"].FirstOrDefault(it =>
+                it.ToUpper().IndexOf("HTTP", StringComparison.Ordinal) != -1 &&
+                it.IndexOf("2.0", StringComparison.Ordinal) != -1) != null)
+            {
+                env[OwinConstants.Opaque.Upgrade] = new UpgradeDelegate(OpaqueUpgradeDelegate);
+            }
+
         }
 
-        private static void EndResponse(DuplexStream socket, Exception ex)
+        #endregion
+
+        /// <summary>
+        /// Completes response by sending result back to the client.
+        /// </summary>
+        /// <param name="client">The client connection.</param>
+        /// <param name="ex">The error occured.</param>
+        private static void EndResponse(DuplexStream client, Exception ex)
         {
-            var msgBytes = Encoding.UTF8.GetBytes(ex.Message);
-            // todo add content type
-            Http11Manager.SendResponse(socket, msgBytes, StatusCode.Code500InternalServerError, "");
-            socket.Flush();
+            Http11Manager.SendResponse(client, Encoding.UTF8.GetBytes(ex.Message), StatusCode.Code500InternalServerError, ""); // TODO content-type ??
+            client.Flush();
         }
 
+        /// <summary>
+        /// Completes response by sending result back to the client.
+        /// </summary>
+        /// <param name="client">The client connection./param>
+        /// <param name="env">Reponse instance in form of OWIN dictionaty.</param>
+        private static void EndResponse(DuplexStream client, IDictionary<string, object> env)
+        {
+            var owinResponse = new OwinResponse(env);
+            
+            byte[] bytes;
+
+            // response body
+            if (owinResponse.Body is MemoryStream)
+            {
+                bytes = (owinResponse.Body as MemoryStream).ToArray();
+            }
+            else
+            {
+                bytes = new byte[0];
+            }
+
+            // response headers
+            var headers = new Dictionary<string, string>();
+            foreach (KeyValuePair<string, string[]> pair in owinResponse.Headers)
+            {
+                var key = pair.Key.ToLowerInvariant();
+
+                // these two are special and passed separatly// TODO move this check to httpmanager
+                if (key == "content-length" || key == "content-type") continue;
+
+                headers[key] = string.Join("\0", pair.Value);
+            }
+
+            Http11Manager.SendResponse(client, bytes, owinResponse.StatusCode, owinResponse.ContentType, headers);
+
+            client.Flush();
+        }
+
+        /// <summary>
+        /// Creates request OWIN respresentation. 
+        /// TODO move to utils or base class since it could be shared across http11 and http2 protocol adapters.
+        /// </summary>
+        /// <param name="method">The request method.</param>
+        /// <param name="scheme">The request scheme.</param>
+        /// <param name="hostHeaderValue">The request host.</param>
+        /// <param name="pathBase">The request base path.</param>
+        /// <param name="path">The request path.</param>
+        /// <param name="requestBody">The body of request.</param>
+        /// <returns>OWIN representation for provided request parameters.</returns>
         private static Dictionary<string, object> CreateOwinEnvironment(string method, string scheme, string hostHeaderValue, string pathBase, string path, byte[] requestBody = null)
         {
             var environment = new Dictionary<string, object>(StringComparer.OrdinalIgnoreCase);
@@ -119,30 +216,6 @@ namespace SocketServer
             environment["owin.ResponseBody"] = new MemoryStream();
             return environment;
         }
-
-        private static void EndResponse(DuplexStream socket, IDictionary<string, object> env)
-        {
-            OwinResponse owinResponse = new OwinResponse(env);
-
-            // TODO constants, better check
-            var bytes = (owinResponse.Body as MemoryStream).ToArray();
-
-            var headers = new Dictionary<string, string>();
-            //headers["Connection"] = "Close";
-
-            foreach (KeyValuePair<string, string[]> pair in owinResponse.Headers)
-            {
-                var key = pair.Key.ToLowerInvariant();
-
-                // these two are special and passed separatly// TODO move this check to httpmanager
-                if (key == "content-length" || key == "content-type") continue;
-
-                headers[key] = string.Join("\0", pair.Value);
-            }
-
-            Http11Manager.SendResponse(socket, bytes, owinResponse.StatusCode, owinResponse.ContentType, headers);
-            
-            socket.Flush();
-        }
+        
     }
 }
--- Shared/SharedProtocol/Http11/Http11Manager.cs
+++ Shared/SharedProtocol/Http11/Http11Manager.cs
@@ -20,55 +20,6 @@ namespace SharedProtocol.Http11
     {
       
    
-        //public static void Http11SendResponse(SecureSocket socket)
-        //{
-        //    string[] headers = GetHttp11Headers(socket);
-        //    string filename = GetFileName(headers);
-
-
-        //    if (headers.Length == 0)
-        //    {
-        //        Http2Logger.LogError("Request headers empty!");
-        //    }
-
-        //    string path = Path.GetFullPath(AssemblyPath + @"\root" + filename);
-        //    string contentType = ContentTypes.GetTypeFromFileName(filename);
-        //    if (!File.Exists(path))
-        //    {
-        //        Http2Logger.LogError("File " + filename + " not found");
-        //        SendResponse(socket, new byte[0], StatusCode.Code404NotFound, contentType);
-        //        socket.Close();
-        //        return;
-        //    }
-
-        //    try
-        //    {
-        //        using (var sr = new StreamReader(path))
-        //        {
-        //            string file = sr.ReadToEnd();
-
-        //            var fileBytes = Encoding.UTF8.GetBytes(file);
-
-        //            int sent = SendResponse(socket, fileBytes, StatusCode.Code200Ok, contentType);
-        //            Http2Logger.LogDebug(string.Format("Sent: {0} bytes", sent));
-        //            Http2Logger.LogInfo("File sent: " + filename);
-
-        //            socket.Close();
-
-        //            if (OnSocketClosed != null)
-        //            {
-        //                OnSocketClosed(null, new SocketCloseEventArgs());
-        //            }
-        //        }
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        var msgBytes = Encoding.UTF8.GetBytes(ex.Message);
-        //        SendResponse(socket, msgBytes, StatusCode.Code500InternalServerError, contentType);
-        //        Http2Logger.LogError(ex.Message);
-        //    }
-        //}
-
         public static int SendResponse(DuplexStream socket, byte[] data, int statusCode, string contentType, Dictionary<string,string> headers = null)
         {
             string initialLine = "HTTP/1.1 " + statusCode + " " + StatusCode.GetReasonPhrase(statusCode) + "\r\n";
@@ -167,6 +118,33 @@ namespace SharedProtocol.Http11
             headers.RemoveAll(String.IsNullOrEmpty);
 
             return headers.ToArray();
+        }
+
+        // TODO find better way
+        public static IDictionary<string, string[]> ParseHeaders(IEnumerable<string> headers)
+        {
+            var dict = new Dictionary<string, string[]>(StringComparer.OrdinalIgnoreCase);
+            foreach (var header in headers)
+            {
+                int colonIndex = header.IndexOf(':');
+                if (colonIndex == -1)
+                {
+                    dict.Add(header, new string[0]);
+                }
+                else
+                {
+                    string headerName = header.Substring(0, colonIndex);
+                    string[] values = header.Substring(colonIndex + 2).Split(','); // colon and space are skipped
+                    for (int i = 0; i < values.Length; ++i)
+                    {
+                        values[i] = values[i].Trim();
+                    }
+
+                    dict.Add(headerName, values);
+                }
+            }
+
+            return dict;
         }
     }
 }
