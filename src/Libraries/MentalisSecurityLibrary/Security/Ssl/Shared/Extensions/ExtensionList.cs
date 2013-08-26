1a2f2e6e22afdaa9a15e95db102f0b698ed1ee2b
sgrebnov <sergei.grebnov@gmail.com>
2013-08-27 00:09:09 +0400

    adds missing files

---
+++ src/Libraries/MentalisSecurityLibrary/Security/Ssl/Shared/Extensions/ExtensionList.cs
@@ -0,0 +1,83 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="ExtensionList.cs" company="Microsoft Open Technologies, Inc.">
+//Copyright © 2002-2007, The Mentalis.org Team
+//Portions Copyright © Microsoft Open Technologies, Inc.
+//All rights reserved.
+//http://www.mentalis.org/ 
+//Redistribution and use in source and binary forms, with or without modification, 
+//are permitted provided that the following conditions are met:
+//- Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer. 
+//- Neither the name of the Mentalis.org Team, 
+//nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+//THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, 
+//INCLUDING, BUT NOT LIMITED TO, 
+//THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. 
+//IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, 
+//INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, 
+//PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; 
+//OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+//OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, 
+//EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+// </copyright>
+//-----------------------------------------------------------------------
+using System;
+using System.Collections.Generic;
+
+using System.IO;
+
+namespace Org.Mentalis.Security.Ssl.Shared.Extensions
+{
+    using Org.Mentalis.Security.BinaryHelper;
+
+	internal class ExtensionList : List<Extension>
+	{
+        public ExtensionList()
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ClientHelloExtensionList"/> class.
+        /// </summary>
+        /// <param name="extensions">The clientHello extensions.</param>
+        public ExtensionList(params Extension[] extensions)
+        {
+            this.AddRange(extensions);
+        }
+
+        public Int16 ExtensionsByteLength
+        {
+            get
+            {
+                Int16 result = 0;
+
+                foreach (var extension in this)
+                    result += extension.GetExtLength();
+
+                return result;           
+            }
+        }
+
+        public T GetExtesionOfType<T>() where T:Extension
+        {
+            foreach (var ext in this)
+            {
+                if (ext is T)
+                    return (T)ext;
+            }
+
+            return default(T);
+        }
+
+        public void WriteExtensions(Stream stream, ConnectionEnd end)
+        {
+            if (this.Count != 0)
+            {
+                byte[] allExtensionsLength = BinaryHelper.Int16ToBytes(this.ExtensionsByteLength);
+                stream.Write(allExtensionsLength, 0, allExtensionsLength.Length);
+
+                foreach (var extension in this)
+                    extension.Write(stream, end);
+            }
+        }
+	}
+}
