2e18870e0ed85a1698ecaf2d71d559aec64bb19b
sgrebnov <sergei.grebnov@gmail.com>
2013-08-26 21:05:16 +0400

    Samples. Adds Static File Server demo project

---
+++ samples/Http2.Owin.Server.StaticFiles/Startup.cs
@@ -0,0 +1,18 @@
+﻿using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Http2.Owin.Server.StaticFiles
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder builder)
+        {
+            builder.UseHttp2();
+            builder.UseStaticFiles("root");
+        }
+    }
+}
