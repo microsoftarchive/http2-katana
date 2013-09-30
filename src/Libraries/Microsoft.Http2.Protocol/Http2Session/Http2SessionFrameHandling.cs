74fabdafc45169f360e29ac5e1197fc9a05031b9
SquirrelFm <squirrelfm@gmail.com>
2013-09-30 18:02:56 +0400

    Removes unnecessary namespaces

--- src/Libraries/Microsoft.Http2.Protocol/Http2Session/Http2SessionFrameHandling.cs
+++ src/Libraries/Microsoft.Http2.Protocol/Http2Session/Http2SessionFrameHandling.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Microsoft.Http2.Protocol.Exceptions;
 using Microsoft.Http2.Protocol.Framing;
 using Microsoft.Http2.Protocol.Utils;
