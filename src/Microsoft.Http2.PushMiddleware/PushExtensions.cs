1e9159c8f8ff8171d43db6c2c37af6e94e882acc
SquirrelFm <squirrelfm@gmail.com>
2013-11-20 17:28:41 +0400

    Some refactorings Fixes Http2.Tests project. Removes reference on unexisting file.

--- src/Microsoft.Http2.PushMiddleware/PushExtensions.cs
+++ src/Microsoft.Http2.PushMiddleware/PushExtensions.cs
@@ -12,21 +12,21 @@ namespace Owin
             return builder.Use(typeof(PushMiddleware));
         }
 
-        public static bool HasRecursion(this Dictionary<String,IList<String>> graph)
+        public static bool HasRecursion(this Dictionary<String, IList<String>> graph)
         {
             return graph.Any(node => IsRecursionByThisHead(node, graph));
         }
 
-        static bool IsRecursionByThisHead(KeyValuePair<String, IList<String>> current
-           , Dictionary<String, IList<String>> wholeHierarchy)
+        private static bool IsRecursionByThisHead(KeyValuePair<String, IList<String>> current,
+                                            Dictionary<String, IList<String>> wholeHierarchy)
         {
-            bool isRecursion = false;
-            foreach (String vertex in current.Value)
+            bool gotRecursion = false;
+            foreach (var vertex in current.Value)
                 if (wholeHierarchy.Keys.Contains(vertex))
                     if (wholeHierarchy[vertex].Contains(current.Key))
-                        isRecursion = true;
+                        gotRecursion = true;
 
-            return isRecursion;
+            return gotRecursion;
         }
 
     }
