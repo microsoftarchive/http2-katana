6c3f9674b26a6c6ffccff2bd9701110b8198b22e
squirrelfm <squirrelfm@gmail.com>
2013-12-16 19:24:50 +0400

    Solves packages conflicts

--- samples/Http2.Owin.StaticFiles.Sample/Startup.cs
+++ samples/Http2.Owin.StaticFiles.Sample/Startup.cs
@@ -20,7 +20,7 @@ namespace Http2.Owin.StaticFiles.Sample
         {
             builder.UseHttp2();
             builder.UsePush();
-            builder.UseStaticFiles("root");
+            builder.UseStaticFiles("/root");
         }
     }
 }
